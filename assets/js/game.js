892f0dd238b01583921f74f315afa6d4cb6484b1
olliedirker <olliedirker@gmail.com>
2022-05-21 13:07:27 -0500

    inital game setup, money added and run away added

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,20 +2,27 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10
+
 console.log(playerName, playerAttack, playerHealth);
+
 var enemyName = "DoctorRobuttnick";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//fighting time
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 };
+//Fight or run
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose!")
+if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "Fight") {
 // player attack phase
 enemyHealth = enemyHealth - playerAttack;
 console.log(
 playerName + " attacked " + enemyName + ".  " + enemyName + " now has " + enemyHealth + " health remaining. "
 );
-//health check
+// enemy health check
 if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!!!")
 } else {
@@ -26,10 +33,21 @@ playerHealth = playerHealth - enemyAttack
 console.log(
     enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining. "
 );
-//enemy health check
+//player health check
 if (playerHealth <= 0) {
     window.alert(playerName + " has died!!")
 } else {
     window.alert(playerName + " still has " + playerHealth + " health left.")
 };
-fight()
+//if you dont want to gladiate
+}else if (promptFight === "skip" || promptFight === "SKIP" || promptFight === 'Skip'){
+    var confirmSkip = window.confirm('Are you sure you do not wish to Gladiate??')
+    if(confirmSkip){
+        window.alert(playerName + " has decided to run from glory. Goodluck next battle!")
+        playerMoney = playerMoney - 2;
+    }
+    // if they want to fight instead run fight function
+    else{fight();}
+} else{
+    window.alert("You must choose a valid option. Please try again!");
+}
